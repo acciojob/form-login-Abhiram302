f7bcb3c9dbb147b3f3b634f673c552172cfc05b0
Abhiram302 <abhiramburra3@gmail.com>
2025-08-03 12:11:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+    const form = document.querySelector('form');
+	const fname = form[0].value;
+	const lname = form[1].value;
+	alert(fname+ lname);
 }
